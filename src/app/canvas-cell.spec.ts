891cfe0bc9fe91396961c63373abcbfea9568edb
Suraj Gharat <mr.surajgharat2@gmail.com>
2021-06-26 15:27:49 +0530

    Fix two unit tests

--- src/app/canvas-cell.spec.ts
+++ src/app/canvas-cell.spec.ts
@@ -2,6 +2,6 @@ import { CanvasCell } from './canvas-cell';
 
 describe('CanvasCell', () => {
   it('should create an instance', () => {
-    expect(new CanvasCell()).toBeTruthy();
+    expect(new CanvasCell(10, 10)).toBeTruthy();
   });
 });
--- src/app/cell-life.spec.ts
+++ src/app/cell-life.spec.ts
@@ -1,7 +1,8 @@
+import { CanvasCell } from './canvas-cell';
 import { CellLife } from './cell-life';
 
 describe('CellLife', () => {
   it('should create an instance', () => {
-    expect(new CellLife()).toBeTruthy();
+    expect(new CellLife(new CanvasCell(10, 20), false)).toBeTruthy();
   });
 });
